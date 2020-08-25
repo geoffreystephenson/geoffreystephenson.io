17e37da5510e69e542349f53dac251af0228ff23
Geoffrey Stephenson <stephenson.geoffrey@gmail.com>
2020-08-25 10:41:13 -0400

    tweak

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
 
 			<main>
 				<h1 className="title">Geoffrey Stephenson</h1>
-				<p className="description text-center"></p>
+				<p className="description"></p>
 			</main>
 
 			<footer></footer>
