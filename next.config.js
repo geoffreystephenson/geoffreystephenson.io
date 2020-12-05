129629c162906ddf76f4a0d7a5d82a9affa014c4
Geoffrey Stephenson <stephenson.geoffrey@gmail.com>
2020-12-05 08:46:23 -0500

    Adding next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	trailingSlash: true,
+	exportPathMap: async function (
+		defaultPathMap,
+		{ dev, dir, outDir, distDir, buildId }
+	) {
+		return {
+			'/': { page: '/' },
+			'/about': { page: '/about' },
+		};
+	},
+};
