71b904cbfc90fac1d73a924fe275a7bbfd5a2241
Geoffrey Stephenson <stephenson.geoffrey@gmail.com>
2021-03-10 22:00:07 -0500

    WIP

--- pages/about.js
+++ pages/about.js
@@ -14,9 +14,10 @@ export default function About() {
 			</Head>
 			<h1>About {process.env.NEXT_PUBLIC_NAME}</h1>
 			<p>Senior Software Engineer &middot; Full Stack Developer</p>
+			<p>Atlanta, GA</p>
 			<h2>
 				<Link href="/">
-					<a>Back to home</a>
+					<a>Back to homepage</a>
 				</Link>
 			</h2>
 		</Layout>
